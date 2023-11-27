ee46b85f2e23c6ffb873488d6fcd1fbded4cc91c
Mattias Vizcarra <m.vizcarra17@hotmail.com>
2023-11-27 16:59:39 -0300

    feat(agregarComponent)Implementacion del componente agregar

--- src/app/productos/main.component.ts
+++ src/app/productos/main.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ListadoComponent],
+  imports: [CommonModule, NavbarComponent, AgregarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
---
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-agregar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './agregar.component.html',
+  styleUrl: './agregar.component.css'
+})
+export class AgregarComponent {
+
+}
