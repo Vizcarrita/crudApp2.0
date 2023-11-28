c1c67ff99948024ba68913fd22e3f3e0b8487248
Mattias Vizcarra <m.vizcarra17@hotmail.com>
2023-11-27 22:14:02 -0300

    feat(tabsComponent)Implementacion del componente tabs

--- src/app/productos/main.component.ts
+++ src/app/productos/main.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+import { TabsComponent } from '../shared/tabs/tabs.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, AgregarComponent],
+  imports: [CommonModule, NavbarComponent, AgregarComponent, TabsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
