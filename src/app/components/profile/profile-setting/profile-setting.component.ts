9438d74ab88e4e3118c9a57f6d964a6dc0cc1dac
Mattias Vizcarra <m.vizcarra17@hotmail.com>
2023-11-27 23:45:52 -0300

    feat(profileSettingComponent)Implementacion del componente configuracion de usuario

---
+++ src/app/components/profile/profile-setting/profile-setting.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-setting',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-setting.component.html',
+  styleUrl: './profile-setting.component.css'
+})
+export class ProfileSettingComponent {
+
+}
--- src/app/productos/main.component.ts
+++ src/app/productos/main.component.ts
@@ -4,11 +4,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { TabsComponent } from '../shared/tabs/tabs.component';
+import { ProfileSettingComponent } from '../components/profile/profile-setting/profile-setting.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, AgregarComponent, TabsComponent],
+  imports: [CommonModule, NavbarComponent, ProfileSettingComponent, TabsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
